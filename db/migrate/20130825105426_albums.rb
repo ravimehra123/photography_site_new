3b67fa091daef27c8a73f075512260651a1c0a3d
vishalsingh <vishal.singh030@gmail.com>
2013-08-25 16:43:32 +0500

    remove migration

--- db/migrate/20130825105426_albums.rb
+++
@@ -1,7 +0,0 @@
-class Albums < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20130825105839_album_images.rb
+++
@@ -1,7 +0,0 @@
-class AlbumImages < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
