b9786f2f72337b56d1db4d0e7ed6382cb940c20c
vishal singh <vishal.singh030@gmail.com>
2013-08-31 20:56:21 +0530

    change admin

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,13 +1,13 @@
 class Admin::DashboardsController < ApplicationController
-	before_filter :authenticate_user!
-	before_filter do
-		if  (current_user and current_user.admin == true)
-		else
-			flash[:notice]="Invalid Login Credentials"
-			session.clear
-			redirect_to root_url
-		end
-	end
+	# before_filter :authenticate_user!
+	# before_filter do
+	# 	if  (current_user and current_user.admin == true)
+	# 	else
+	# 		flash[:notice]="Invalid Login Credentials"
+	# 		session.clear
+	# 		redirect_to root_url
+	# 	end
+	# end
 	layout "admin"
 	def index
 	end
