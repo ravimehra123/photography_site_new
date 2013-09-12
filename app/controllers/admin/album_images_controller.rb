dc265b7ce66c3dfca7d6ec66c19194dc10e3362f
Shoaib Malik <shoaib.malik@addvalsolutions.com>
2013-09-13 00:20:37 +0530

    bug fixed

--- app/controllers/admin/album_images_controller.rb
+++ app/controllers/admin/album_images_controller.rb
@@ -1,16 +1,6 @@
 class Admin::AlbumImagesController < ApplicationController
-  # before_filter :user_role_auth
- # before_filter :authenticate_user!
- #  before_filter do
- #    if  (current_user and current_user.admin == true)
- #    else
- #      flash[:notice]="Invalid Login Credentials"
- #      session.clear
- #      redirect_to root_url
- #    end
- #  end
  before_filter :auth_admin
- end
+ 
   layout "admin"
   def index
     @album_images = AlbumImage.all
