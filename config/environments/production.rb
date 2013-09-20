4ddc00748ffbb7cabe8abce49cf1f006864edb28
Shoaib Malik <shoaib.malik@addvalsolutions.com>
2013-09-20 08:39:52 +0530

    undo all changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,9 @@ NewProject::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
-  config.serve_static_assets = false
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   # config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -31,12 +30,7 @@ NewProject::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = true
-
-  config.assets.compile = true
-
-  config.assets.digest = true
-
+  config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
